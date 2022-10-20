0e99531eda7f75a2e8f71f344126d1b4f87c0964
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-10-20 16:44:57 +0200

    Refactored a little bit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,6 +2,23 @@ import React, { useState } from 'react'
 
 const Counter = () => {
 	const [count, setCount] = useState(0)
+	const [history, setHistory] = useState([])
+
+	const handleDecrement = (e) => {
+		const number = Number(e.target.innerText.split('').slice(1).join(''))
+
+		setCount(count - number)
+	}
+
+	const handleIncrement = (e) => {
+		const number = Number(e.target.innerText.split('').slice(1).join(''))
+
+		setCount(count + number)
+	}
+
+	const undo = () => {}
+
+	const redo = () => {}
 
 	return (
 		<div className='counter-container'>
@@ -10,31 +27,33 @@ const Counter = () => {
 			</header>
 
 			<div className='undo-redo-buttons'>
-				<button className='btn'>Undo</button>
+				<button onClick={undo} className='btn'>
+					Undo
+				</button>
 				<button className='btn'>Redo</button>
 			</div>
 
 			<div className='counter-buttons'>
 				<button
 					className='btn'
-					onClick={() => {
-						setCount(count - 100)
+					onClick={(e) => {
+						handleDecrement(e)
 					}}
 				>
 					-100
 				</button>
 				<button
 					className='btn'
-					onClick={() => {
-						setCount(count - 10)
+					onClick={(e) => {
+						handleDecrement(e)
 					}}
 				>
 					-10
 				</button>
 				<button
 					className='btn'
-					onClick={() => {
-						setCount(count - 1)
+					onClick={(e) => {
+						handleDecrement(e)
 					}}
 				>
 					-1
@@ -44,24 +63,24 @@ const Counter = () => {
 
 				<button
 					className='btn'
-					onClick={() => {
-						setCount(count + 1)
+					onClick={(e) => {
+						handleIncrement(e)
 					}}
 				>
 					+1
 				</button>
 				<button
 					className='btn'
-					onClick={() => {
-						setCount(count + 10)
+					onClick={(e) => {
+						handleIncrement(e)
 					}}
 				>
 					+10
 				</button>
 				<button
 					className='btn'
-					onClick={() => {
-						setCount(count + 100)
+					onClick={(e) => {
+						handleIncrement(e)
 					}}
 				>
 					+100
