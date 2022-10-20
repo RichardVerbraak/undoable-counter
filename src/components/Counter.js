55570db384152820855453d310fe22cac56c94cb
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-10-20 16:22:00 +0200

    Just making things work before refactoring

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,21 +9,63 @@ const Counter = () => {
 				<h1>Undoable Counter</h1>
 			</header>
 
-			<div>
-				<p>Undo</p>
-				<p>Redo</p>
+			<div className='undo-redo-buttons'>
+				<button className='btn'>Undo</button>
+				<button className='btn'>Redo</button>
 			</div>
 
-			<div>
-				<p>-100</p>
-				<p>-10</p>
-				<p>-1</p>
+			<div className='counter-buttons'>
+				<button
+					className='btn'
+					onClick={() => {
+						setCount(count - 100)
+					}}
+				>
+					-100
+				</button>
+				<button
+					className='btn'
+					onClick={() => {
+						setCount(count - 10)
+					}}
+				>
+					-10
+				</button>
+				<button
+					className='btn'
+					onClick={() => {
+						setCount(count - 1)
+					}}
+				>
+					-1
+				</button>
 
 				<p className='count'>{count}</p>
 
-				<p>+1</p>
-				<p>+10</p>
-				<p>+100</p>
+				<button
+					className='btn'
+					onClick={() => {
+						setCount(count + 1)
+					}}
+				>
+					+1
+				</button>
+				<button
+					className='btn'
+					onClick={() => {
+						setCount(count + 10)
+					}}
+				>
+					+10
+				</button>
+				<button
+					className='btn'
+					onClick={() => {
+						setCount(count + 100)
+					}}
+				>
+					+100
+				</button>
 			</div>
 
 			<div className='history'>
